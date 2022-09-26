00c35a91074d3be157fccdadfc3b98cce962250c
Akinwole Adam <akinwoleadam@gmail.com>
2022-09-26 02:28:00 +0100

    modified settings.py file

--- Todoapp/settings.py
+++ Todoapp/settings.py
@@ -132,6 +132,8 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
