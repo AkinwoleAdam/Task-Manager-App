39c092c98191f3fffa3e0c4ca0ef6566525fa24b
Akinwole Adam <akinwoleadam@gmail.com>
2022-10-08 15:24:27 +0100

    Static files configuration and final deployment

--- Todoapp/settings.py
+++ Todoapp/settings.py
@@ -135,11 +135,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL ='/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
